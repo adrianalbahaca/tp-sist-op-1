5a21fa8d53b197cf634c6ab2dad75f8a13aeed32
Adrian Albahaca <adrian.albahaca@gmail.com>
2026-06-19 12:51:35 -0300

    Nombre anotado para el rol -> Notas para entender el problema

---
+++ agente_c/include/resource_manager.h
@@ -0,0 +1,14 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+
+/**
+ * TODO:
+ * 1. Planificar la estructura de un recurso, qué contiene y cómo se guardará
+ * 2. Crear las funciones de manejo para cada job solicitado
+ * 3. Crear la cola FIFO de solicitudes pendientes
+ * 4. Entender que pasa cuando hay desconexion, y cómo lidiar con ello adecuadamente
+ * 5. Crear funciones de parseo que manejen el lenguaje de cada recurso
+ * 6. Entender la tabla de jobs para detectar desconexiones
+ */
+
+#endif
