9fa782f47869078704d1e48d868b3c1b80bc582b
Adrian Albahaca <adrian.albahaca@gmail.com>
2026-06-19 22:51:53 -0300

    Funciones de creación y eliminación de resourse manager hechas

--- agente_c/src/resource_manager.c
+++ agente_c/src/resource_manager.c
@@ -1,4 +1,7 @@
 #include "../include/resource_manager.h"
+#include <stdlib.h>
+#include <string.h>
+
 #define TAM_TABLA_JOBS 71 // Se selecciona un numero primo chico por cuestiones de optimización
 
 typedef enum {
@@ -53,6 +56,9 @@ typedef struct {
     ColaPendingRequest pending_gpu;
 } ResourceManager;
 
+/**
+ * El gestor de recursos se define estáticamente para trabajar con memoria dinámica lo menos posible y evitar complicaciones
+ */
 static ResourceManager manager;
 
 /**
@@ -61,11 +67,47 @@ static ResourceManager manager;
  * ======================================================================================
  */
 
+static void queue_init(ColaPendingRequest* c) {
+    c->top = NULL;
+    c->bottom = NULL;
+}
+
+static void queue_destroy(ColaPendingRequest* c) {
+    PendingRequest* curr = c->top;
+    while (curr != NULL) {
+        PendingRequest* next = curr->sig;
+        free(curr);
+        curr = next;
+    }
+    c->top = NULL;
+    c->bottom = NULL;
+}
+
 /**
  * ======================================================================================
  * Tabla de Jobs
  * ======================================================================================
  */
+
+static void table_init(Job** j) {
+    memset(j, 0, TAM_TABLA_JOBS * sizeof(*j));
+    return;
+}
+
+static void table_destroy(Job **j) {
+    for (int i = 0; i < TAM_TABLA_JOBS; i++) {
+        Job* job = j[i];
+
+        // Cada bucket es una lista enlazada, por lo que es esencial eliminar cada uno de sus elementos
+        while (job != NULL) {
+            Job* next =  job->allocations;
+            free(job);
+            job = next;
+        }
+
+        j[i] = NULL;
+    }
+}
 
 /**
  * ======================================================================================
@@ -83,10 +125,18 @@ void manager_init(int cpu, int mem, int gpu) {
     queue_init(&manager.pending_cpu);
     queue_init(&manager.pending_mem);
     queue_init(&manager.pending_gpu);
+
+    table_init(&manager.tabla_jobs);
+
+    return;
 }
 
 void manager_destroy() {
     queue_destroy(&manager.pending_cpu);
     queue_destroy(&manager.pending_mem);
     queue_destroy(&manager.pending_gpu);
+
+    table_destroy(&manager.tabla_jobs);
+    
+    return;
 }
